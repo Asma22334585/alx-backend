4e4744d91dc73a5608c9079915c4e6987c966e44
Asma22334585 <guesmihadil0@gmail.com>
2024-07-24 18:07:17 +0000

    my commit

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" LIFO caching """
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """doc doc doc"""
+
+    def __init__(self):
+        """init method"""
+        super().__init__()
+
+    def put(self, key, item):
+        """tem value for the key key"""
+        if key and item:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                discard = list(self.cache_data.keys())[-1]
+                self.cache_data.pop(discard)
+                print("DISCARD: {}".format(discard))
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """value in self.cache_data linked to key"""
+        return self.cache_data.get(key)
