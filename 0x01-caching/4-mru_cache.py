06d63c6123c7f6e65376b5898e89a37939b88836
Asma22334585 <guesmihadil0@gmail.com>
2024-07-26 14:49:00 +0000

    my commit

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+""" LRU Caching """
+
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """ class LRU """
+    def __init__(self):
+        """ Init method """
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """ item value for the key key """
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                discard = self.order.pop()
+                del self.cache_data[discard]
+                print("DISCARD: {}".format(discard))
+            self.cache_data[key] = item
+            self.order.append(key)
+        else:
+            return None
+
+    def get(self, key):
+        """ self.cache_data linked to key """
+        if key is not None:
+            if key in self.cache_data:
+                self.order.remove(key)
+                self.order.append(key)
+                return self.cache_data[key]
+        return None
