9ceaeacddd8db56c5b02a16dc06f309bca890716
Asma22334585 <guesmihadil0@gmail.com>
2024-07-26 14:45:58 +0000

    my commit

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,29 +1,33 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """ LRU Caching """
-BaseCaching = __import__("base_caching").BaseCaching
+
+from base_caching import BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """class LRU"""
-
+    """ class LRU """
     def __init__(self):
-        """init method"""
+        """ Init method """
         super().__init__()
         self.order = []
 
     def put(self, key, item):
-        """item value for the key key"""
-        if key and item:
+        """ item value for the key key """
+        if key is not None and item is not None:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
                 discard = self.order.pop(0)
-                self.cache_data.pop(discard)
+                del self.cache_data[discard]
                 print("DISCARD: {}".format(discard))
             self.cache_data[key] = item
             self.order.append(key)
+        else:
+            return None
 
     def get(self, key):
-        """self.cache_data linked to key"""
-        if key in self.cache_data:
-            self.order.remove(key)
-            self.order.append(key)
-            return self.cache_data.get(key)
+        """ self.cache_data linked to key """
+        if key is not None:
+            if key in self.cache_data:
+                self.order.remove(key)
+                self.order.append(key)
+                return self.cache_data[key]
+        return None
