7f3f1a7f6919831f1ed0b7e06a5d30960bd73c23
Asma22334585 <guesmihadil0@gmail.com>
2024-07-30 20:31:23 +0000

    my commit

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" Basic Babel setup """
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config(object):
+    """doc doc doc"""
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route("/")
+def index():
+    """ index """
+    return render_template("1-index.html")
