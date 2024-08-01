753c4cc5c5a6c426de25bef58a1480c222da492c
Asma22334585 <guesmihadil0@gmail.com>
2024-08-01 22:32:53 +0000

    my commit

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" Parametrize templates """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config(object):
+    """ config class"""
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """ get locale"""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route("/")
+def index() -> str:
+    """ index """
+    return render_template("2-index.html")
